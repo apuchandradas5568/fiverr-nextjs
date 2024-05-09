e565c04e54e823bce6e11f3fbbafc26bef1bbcaa
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-05-09 11:52:00 +0530

    added categories and subcategories code and changed the dirctory of middleware file

--- src/app/seller/[username]/manage-gigs/create/page.tsx
+++ src/app/seller/[username]/manage-gigs/create/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { useMutation } from "convex/react";
 import { CreateForm } from "./_components/create-form";
+import { api } from "../../../../../../convex/_generated/api";
+import { useEffect } from "react";
 
 interface CreateGigProps {
   params: {
@@ -9,9 +12,17 @@ interface CreateGigProps {
 }
 
 const CreateGig = ({ params }: CreateGigProps) => {
+
+    const insertCategories = useMutation(api.seedCategories.create)
+    
+    useEffect(() => {
+        insertCategories({})
+    }, [])
+
+
+
   return (
-    <div>
-      <h1>Create Gig</h1>
+    <div className="flex justify-center p-8">
       <CreateForm username={params.username} />
     </div>
   );
